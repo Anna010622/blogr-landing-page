cad128cae03a037ba9db446da6f62accfa6165fb
Anna Trifonova <3809322999@ukr.net>
2025-05-28 21:59:46 +0300

    add the “open-close” functionality to the mobile navigation bar

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,48 @@
 const modal = document.querySelector('#mobile-menu');
-const toggleModalBtn = document.querySelector('#mobile-menu + button');
+const toggleModalBtn = document.querySelector('[data-toggle-mob-menu');
+const body = document.querySelector('body');
 
-toggleModalBtn.addEventListener('click', () => {
-	modal.toggleAttribute('hidden');
+toggleModalBtn.addEventListener('click', e => {
+	e.stopPropagation();
+	if (toggleModalBtn.getAttribute('aria-expanded') === 'false') {
+		openMenu();
+	} else {
+		closeMenu();
+	}
 });
+
+function closeMenu() {
+	modal.setAttribute('hidden', '');
+	toggleModalBtn.setAttribute('aria-expanded', false);
+
+	body.removeEventListener('click', handleClickOutside);
+	document.removeEventListener('keydown', handleClickEsc);
+	modal.removeEventListener('click', handleClickOnMenu);
+}
+
+function openMenu() {
+	modal.removeAttribute('hidden');
+	toggleModalBtn.setAttribute('aria-expanded', true);
+
+	body.addEventListener('click', handleClickOutside);
+	document.addEventListener('keydown', handleClickEsc);
+	modal.addEventListener('click', handleClickOnMenu);
+}
+
+function handleClickEsc(event) {
+	if (event.key === 'Escape') {
+		closeMenu();
+	}
+}
+
+function handleClickOutside() {
+	closeMenu();
+}
+
+function handleClickOnMenu(event) {
+	event.stopPropagation();
+
+	if (event.target.hasAttribute('data-active-el')) {
+		closeMenu();
+	}
+}
